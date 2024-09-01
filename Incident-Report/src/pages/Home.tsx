41858e45ade17d6e39b3a2138561753808334eb5
Abhishek <abui8994ty@gmail.com>
2024-09-01 18:31:38 +0530

    short path change chenge

--- Incident-Report/src/pages/Home.tsx
+++ Incident-Report/src/pages/Home.tsx
@@ -7,18 +7,18 @@ export function Home() {
       <section className="mx-auto flex flex-col items-center justify-center min-h-[90vh]">
         <h1 className="text-[3em] md:text-[4em] lg:text-[7em] flex flex-col gap-5 text-center font-bold font-neueMachinaBold text-balance md:leading-[auto] lg:leading-tight text-black">
           <span className="bg-orange-400 lg:pt-5 px-2 leading-tight">
-           Report Your
+          Record Attentendence
           </span>{" "}
           <span>
-            <span className="bg-fuchsia-500 pt-5 px-2">Road</span>{" "}
-            <span className="bg-fuchsia-500 pt-5 px-2">Incidents</span>
+            <span className="bg-fuchsia-500 pt-5 px-2">Attest</span>{" "}
+            <span className="bg-fuchsia-500 pt-5 px-2">Attendence</span>
           </span>
         </h1>
         <p className="text-[1.5em] md:text-[2em] mt-4 text-center font-bold">
           here....
         </p>
         <div className="flex gap-5">
-          <button onClick={()=> navigate("/report")} className="mt-10 mx-auto flex gap-2 px-6">
+          <button onClick={()=> navigate("/a")} className="mt-10 mx-auto flex gap-2 px-6">
            Get Started 
           </button>
         </div>
